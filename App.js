faf05961d5d6972ee8703e282db94ae2087cf669
fadlan.yamisok <fadlan.zunima@yamisok.com>
2021-12-24 15:17:20 +0700

    feat: screen welcome

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/screen/Home';
@@ -13,19 +14,22 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="SplashScreen"
-        screenOptions={{headerShown: false}}>
-        <Stack.Screen name="SplashScreen" component={SplashScreen} />
-        <Stack.Screen name="SignInNSignUp" component={SignInNSignUp} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#8C96FF" />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="SplashScreen"
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name="SplashScreen" component={SplashScreen} />
+          <Stack.Screen name="SignInNSignUp" component={SignInNSignUp} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Profile" component={Profile} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
--- src/screen/Welcome.js
+++ src/screen/Welcome.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from 'react-native';
 import {useNavigation, StackActions} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function Welcome() {
   const navigation = useNavigation();
-  
+
   const removeValue = async () => {
     try {
       await AsyncStorage.removeItem('@storage_user');
@@ -21,9 +28,41 @@ function Welcome() {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => removeValue()}>
+      {/* <TouchableOpacity onPress={() => removeValue()}>
         <Text>Logout</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
+      <View style={styles.wrapperLogo}>
+        <Image
+          style={styles.iconStarted}
+          source={require('../assets/img/logo-app-started.png')}
+          resizeMode="contain"
+        />
+      </View>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={styles.textHi}>Hi Afsar, Welcome </Text>
+        <Text style={styles.silent}>to Silent Moon</Text>
+        <Text style={styles.desc}>
+          Explore the app, Find some peace of mind to prepare for meditation.
+        </Text>
+      </View>
+      <View style={{position: 'relative'}}>
+        <Image
+          style={styles.meditation}
+          source={require('../assets/img/image-meditation.png')}
+          resizeMode="contain"
+        />
+        <View style={styles.wraperBtn}>
+          <TouchableOpacity style={styles.btnStarted}>
+            <Text> GET STARTED </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
@@ -32,6 +71,61 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#8C96FF',
+  },
+  wrapperLogo: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: Dimensions.get('window').width,
+    marginBottom: 20,
+  },
+  iconStarted: {
+    width: Dimensions.get('window').width * 0.4,
+    height: Dimensions.get('window').width * 0.25,
+  },
+  textHi: {
+    color: '#FFECCC',
+    fontSize: 30,
+    letterSpacing: 0.01,
+    fontWeight: 'bold',
+  },
+  silent: {
+    color: '#FFECCC',
+    fontSize: 24,
+    letterSpacing: 0.01,
+    marginBottom: 30,
+  },
+  desc: {
+    textAlign: 'center',
+    marginHorizontal: 23,
+    fontWeight: '300',
+    fontSize: 16,
+    color: '#EBEAEC',
+  },
+  meditation: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').width * 1.5,
+  },
+  btnStarted: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 374,
+    height: 63,
+    fontSize: 14,
+    color: '#3F414E',
+    backgroundColor: '#EBEAEC',
+    borderRadius: 38,
+  },
+  wraperBtn: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: Dimensions.get('window').width,
+    top: Dimensions.get('window').width * 1,
+    zIndex: 1,
   },
 });
 
